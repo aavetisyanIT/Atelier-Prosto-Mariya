8ed6b23b7cff0907f7990a0bfad15a703c04ac70
aavetisyanit <aavetisyanit@gmail.com>
2021-01-27 12:18:59 -0600

    Implement confirmation/error toast message

--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 
 export default function ContactUs() {
-	const sendEmail = (e) => {
+	const [toast, setToast] = useState('');
+	const [toastMsg, setToastMsg] = useState('');
+
+	const toggleToast = (msg, mode) => {
+		setToastMsg(msg);
+		setToast(`toast toast--${mode} toast--visible`);
+		setTimeout(() => setToast('toast'), 3000);
+	};
+	const sendEmail = e => {
 		e.preventDefault();
 
 		emailjs
@@ -12,20 +20,18 @@ export default function ContactUs() {
 				e.target,
 				'user_JZLigJLWkqlOvo1jwrS0M',
 			)
-			.then(
-				(result) => {
-					console.log(result.text);
-				},
-				(error) => {
-					console.log(error.text);
-				},
-			);
+			.then(() => toggleToast('Email was sent', 'success'))
+			.catch(error => {
+				console.log(error.text);
+				toggleToast('Sorry there was a problem', 'error');
+			});
 		e.target.reset();
 	};
 
 	return (
 		<div>
-			<h1>Contant Us</h1>
+			<h1>Contact Us</h1>
+			<div className={toast}>{toastMsg}</div>
 			<div className='formContainer'>
 				<div className='form'>
 					{/* Form */}
@@ -82,11 +88,12 @@ export default function ContactUs() {
 				</div>
 				<div className='formText'>
 					<h4>
-						We know that getting your garments tailored can get a little
-						confusing. If you have any questions, please send Atelier
-						Prosto Mariya a message! I am happy to get back to you as soon
-						as I can. You can also call me at (773) 603-3396 or stop by
-						our shop Monday through Friday between 9 a.m. and 5 p.m. I
+						We know that getting your garments tailored can
+						get a little confusing. If you have any questions,
+						please send Atelier Prosto Mariya a message! I am
+						happy to get back to you as soon as I can. You can
+						also call me at (773) 603-3396 or stop by our shop
+						Monday through Friday between 9 a.m. and 5 p.m. I
 						can’t wait to meet you!
 					</h4>
 				</div>
@@ -96,68 +103,88 @@ export default function ContactUs() {
 					<h2>WHY US?</h2>
 				</header>
 				<p>
-					If you’ve never used my services before, you’re probably
-					wondering: what’s so different about Atelier Prosto Mariya? Why
-					are my services better than any other tailor in the Chicago area?
-					My experience and commitment to customer satisfaction are two of
-					the biggest reason why you should choose me over my competitors.
+					If you’ve never used my services before, you’re
+					probably wondering: what’s so different about Atelier
+					Prosto Mariya? Why are my services better than any
+					other tailor in the Chicago area? My experience and
+					commitment to customer satisfaction are two of the
+					biggest reason why you should choose me over my
+					competitors.
 				</p>
 				<div className='features'>
 					<article>
 						<div className='content'>
-							<h3>The Most Passionate Tailor in the Chicago Area</h3>
+							<h3>
+								The Most Passionate Tailor in the
+								Chicago Area
+							</h3>
 							<p>
-								What makes me better than all the other tailors out
-								there? It’s simple: my passion for sewing. You won’t
-								find my level of experience from any other local tailor.
-								I’ve been doing this for over 30 years, and I’ve never
-								loved it more! Sewing is in my blood. I do it, just like
-								my mother did it, and I am passing my profession along
-								to my daughter. Through all three generations, we’ve
-								perfected the art of sewing. Even after three-plus
-								decades of perfecting my craft, I still am always trying
-								to learn new techniques and get even better.
+								What makes me better than all the other
+								tailors out there? It’s simple: my
+								passion for sewing. You won’t find my
+								level of experience from any other local
+								tailor. I’ve been doing this for over 30
+								years, and I’ve never loved it more!
+								Sewing is in my blood. I do it, just
+								like my mother did it, and I am passing
+								my profession along to my daughter.
+								Through all three generations, we’ve
+								perfected the art of sewing. Even after
+								three-plus decades of perfecting my
+								craft, I still am always trying to learn
+								new techniques and get even better.
 							</p>
 						</div>
 					</article>
 					<article>
 						<div className='content'>
 							<h3>
-								Committed to Consistently Delivering Customer
-								Satisfaction
+								Committed to Consistently Delivering
+								Customer Satisfaction
 							</h3>
 							<p>
-								The Atelier Prosto Mariya is committed to making every
-								customer satisfied with my work. I do whatever it takes
-								to make you happy. No matter what service you want from
-								us, you know exactly the quality you’ll be getting when
-								you pick up your finished clothing. I understand that
-								clothes are more than just clothes. They hold
-								sentimental value and play a huge role in your
-								self-esteem. In fact, that’s part of the reason I
-								started my business! I want to make clothing that helps
-								people feel great. And when a person’s clothing no
-								longer does that for them, I want to help them regain
-								that special feeling. You can trust me to take care of
-								your garments. When I’m done with your items, they’ll be
-								in their very best condition. You’ll never feel more
-								fabulous than when you’re wearing clothing items that
-								have been tailored by the Atelier Prosto Mariya.
+								The Atelier Prosto Mariya is committed
+								to making every customer satisfied with
+								my work. I do whatever it takes to make
+								you happy. No matter what service you
+								want from us, you know exactly the
+								quality you’ll be getting when you pick
+								up your finished clothing. I understand
+								that clothes are more than just clothes.
+								They hold sentimental value and play a
+								huge role in your self-esteem. In fact,
+								that’s part of the reason I started my
+								business! I want to make clothing that
+								helps people feel great. And when a
+								person’s clothing no longer does that
+								for them, I want to help them regain
+								that special feeling. You can trust me
+								to take care of your garments. When I’m
+								done with your items, they’ll be in
+								their very best condition. You’ll never
+								feel more fabulous than when you’re
+								wearing clothing items that have been
+								tailored by the Atelier Prosto Mariya.
 							</p>
 						</div>
 					</article>
 
 					<article>
 						<div className='content'>
-							<h3>Feel Good About Where Your Dollars Are Going</h3>
+							<h3>
+								Feel Good About Where Your Dollars Are
+								Going
+							</h3>
 							<p>
-								Helping Ukraine is close to my heart because of my
-								Ukrainian roots. That’s why I make it a point to donate
-								to the Ukrainian National Women’s League of America.
-								This non-profit organization helps unite women of
-								Ukrainian descent. When your paying for my services,
-								you’re making a tangible difference in the lives of
-								these women.
+								Helping Ukraine is close to my heart
+								because of my Ukrainian roots. That’s
+								why I make it a point to donate to the
+								Ukrainian National Women’s League of
+								America. This non-profit organization
+								helps unite women of Ukrainian descent.
+								When your paying for my services, you’re
+								making a tangible difference in the
+								lives of these women.
 							</p>
 						</div>
 					</article>
@@ -165,10 +192,11 @@ export default function ContactUs() {
 						<div className='content'>
 							<h3>Choose Atelier Prosto Mariya</h3>
 							<p>
-								Is it time to take your beloved clothing items to a
-								tailor? Make an appointment with the Atelier Prosto
-								Mariya! You won’t find a tailor in the Chicago area with
-								more passion or experience!
+								Is it time to take your beloved clothing
+								items to a tailor? Make an appointment
+								with the Atelier Prosto Mariya! You
+								won’t find a tailor in the Chicago area
+								with more passion or experience!
 							</p>
 						</div>
 					</article>
