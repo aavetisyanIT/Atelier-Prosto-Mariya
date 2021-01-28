afd7983823574dfcdb83bd04adaf94d520d7acdc
aavetisyanit <aavetisyanit@gmail.com>
2021-01-27 21:32:35 -0600

    Add content to helpfulArticles page

--- pages/helpfulArticles.js
+++ pages/helpfulArticles.js
@@ -2,9 +2,171 @@ import React from 'react';
 
 export default function HelpfulArticles() {
 	return (
-		<div>
-			<h1>Helpful Articles</h1>
+		<section>
+			<header className='main'>
+				<h1>Helpful Articles</h1>
+			</header>
+			<h2 id='tailorServices'>
+				6 Tailor Services That Can Benefit Your Wardrobe{' '}
+			</h2>
+			<h4>
+				A lot of people think that a tailor’s only skills are
+				hemming garments and adjusting a dress’s waistline. But an
+				expert tailor can do so much more than that! Professional
+				tailor services can help you hold onto your favorite pieces
+				in your wardrobe for a longer period of time. They can also
+				take a piece of clothing you simply like and transform it
+				into something you love. Here are 6 ways that using a tailor
+				can help you make the most of every item in your closet!
+			</h4>
+			<h2>1. Raise Your Neckline for Increased Modesty</h2>
+			<p className='paragraph'>
+				Does a certain shirt or dress have you showing off a little
+				more than you’d like? Wearing an undershirt may seem like an
+				easy solution, but sometimes that can ruin the look of your
+				outfit. Get the neckline raised instead! A tailor will keep
+				the integrity of your top while helping you only show off
+				what you want to.
+			</p>
+			<h2>2. Change the Neckline Style of a Shirt or Dress</h2>
+			<p className='paragraph'>
+				We all have our favorite necklines that we think look best
+				on us and make us feel the most comfortable. But not every
+				shirt that catches your eye will have your perfect neckline.
+				The right tailor can make sure you get the neckline that’s
+				best for you on the top or dress that you love! You can get
+				the collar on a shirt removed or transform a crew neck into
+				a boatneck.{' '}
+			</p>
+			<h2>3. Shorten Your Shirt for a More Flattering Look</h2>
+			<p className='paragraph'>
+				A long shirt can look awkward, especially if you have a
+				shorter torso. Shirts that are too long are unflattering and
+				sometimes uncomfortable. A tailor can shorten your top to
+				the length that’s perfect for you, whether that’s a crop top
+				or just a standard shirt length. This alteration results
+				will be the most flattering silhouette possible!
+			</p>
+			<h2>4. Add a Zipper for an Easier Fit</h2>
+			<p className='paragraph'>
+				Sometimes getting into a form-fitting dress can be a real
+				struggle. You might think this is just the price you’ll have
+				to pay for looking good. But an expert tailor can help you
+				change that by adding a zipper. This will make getting
+				dressed up easier and faster. You won’t have to stress as
+				much about sucking it in!
+			</p>
+			<h2>
+				5. Shorten Dress Straps to Eliminate the Need for Pulling
+			</h2>
+			<p className='paragraph'>
+				When a dress’s straps don’t fit perfectly, you end up
+				spending the entire time you’re wearing it pulling up the
+				straps. This isn’t a practical solution because it doesn’t
+				actually fix anything. It’s annoying, and it looks wrong. It
+				is possible to have straps that fit correctly. All you have
+				to do is take your dress to a tailor to shorten them.
+			</p>
+			<h2>6. Amp up the Style on Your Leather Jacket</h2>
+			<p className='paragraph'>
+				A tailor can do more than just size alterations.
+				Professional tailor services will also include making your
+				garments even more stylish. Working on leather takes extra
+				skill. But if you have the right tailor, your leather jacket
+				can look even cooler with all the pockets, zippers, buttons,
+				and details you want!
+			</p>
+			<h2>Get the Best Tailor Services from Atelier Prosto Mariya!</h2>
+			<h4>
+				Get all these alterations and more when you choose Atelier
+				Prosto Mariya! Our expert tailoring work is flawless! You
+				won’t find anyone in the area who’s more experienced or
+				delivers better results. Contact us today!{' '}
+			</h4>
+
 			<hr className='major' />
-		</div>
+			<h2 id='clothingSecrets'>
+				5 Secrets to Prolonging the Life of Your Clothing
+			</h2>
+			<h4>
+				If you’re not caring for your clothing properly, you’re
+				hurting your bank account. You’ll have to part with items
+				you love more quickly and go clothes shopping more
+				frequently. But it doesn’t have to be this way. Here are
+				five tips for prolonging the life of your clothing and
+				minimizing the need for clothing repairs.
+			</h4>
+			<h2>1. Read Your Fabric Care Labels</h2>
+			<p className='paragraph'>
+				Most of us ignore the fabric care instructions that come on
+				the tags of our clothing. But in doing so, you’re actually
+				decreasing the lifespan of these items. If a garment says
+				it’s dry clean only, you shouldn’t be washing it at home.
+				Going to the drycleaners can feel like a chore, but if it
+				means your clothing lasts longer, it’s worth it!
+				<br />
+				<br />
+				Some clothing will not require dry cleaning, but you may
+				need to hand wash it. You can find other special
+				instructions on the fabric care label such as the ideal
+				temperature at which you should wash the fabric.
+			</p>
+			<h2>2. Sort Your Clothes Before You Do Laundry</h2>
+
+			<p className='paragraph'>
+				When it’s time to do laundry, it can be tempting to just
+				throw everything into the machine at once. But this is the
+				fastest way to wear out some of your favorite clothing
+				items. If you wash your zip-up jackets with your delicate
+				blouses, you run the risk of damaging them.
+				<br />
+				<br />
+				When your items have different cleaning instructions, they
+				shouldn’t be mixed together. Always separate your items
+				before you do a load of laundry. You’ll enjoy longer-lasting
+				clothing!
+			</p>
+			<h2>3. Protect Delicates with a Garment Bag</h2>
+
+			<p className='paragraph'>
+				Your delicate items are just that: delicate. You should be
+				treating them differently than other items in your wardrobe.
+				The best thing you can do to protect your lingerie, baby
+				clothing, sweaters, and other delicate items is putting them
+				in a mesh bag before washing them and keeping them in the
+				garment bag in the dryer. Garment bags are affordable and
+				easy to find. You can purchase them in a multi-pack online.
+			</p>
+			<h2>4. Wear Your Work Clothing Less Frequently</h2>
+
+			<p className='paragraph'>
+				The most expensive articles of clothing you own are probably
+				the ones you wear to work. Decrease the length of time you
+				wear these by taking them off as soon as you get home. This
+				will also protect your good blouses and suits from stains,
+				snags, and tears. This will definitely help prolong the life
+				of your professional clothing.
+			</p>
+			<h2>5. Have Your Tailor on Speed-Dial</h2>
+
+			<p className='paragraph'>
+				Okay, maybe having your tailor on speed-dial is a little
+				extreme. But once you actually start taking advantage of
+				your tailor’s services, you’ll get a lot more longevity out
+				of your wardrobe. When your clothing rips or tears, a
+				tailor’s clothing repair service can make them good as new!
+				If you change sizes or just don’t love the way certain
+				articles of clothing fit, don’t throw them out. Take them to
+				a tailor! Your wallet will thank you.
+			</p>
+			<h4>
+				Don’t have a tailor you trust in the area? Contact Atelier
+				Prosto Mariya! My wide variety of experience in repairing
+				and preserving your best garments can be a huge asset to
+				your wardrobe. You’ll be amazed by the results! Call me
+				today!
+			</h4>
+			<hr className='major' />
+		</section>
 	);
 }
--- pages/services.js
+++ pages/services.js
@@ -2,32 +2,37 @@ import React from 'react';
 
 export default function Services() {
 	return (
-		<div className='inner'>
+		<section className='inner'>
 			<h1>Services</h1>
 			<h4>
-				At Atelier Prosto Mariya, I offer comprehensive tailoring services
-				to help you with all your clothing needs. Because every tailor shop
-				provides different services, it’s essential to know what a shop can
-				and can’t do before you drop off your clothing to be altered.
+				At Atelier Prosto Mariya, I offer comprehensive tailoring
+				services to help you with all your clothing needs. Because
+				every tailor shop provides different services, it’s
+				essential to know what a shop can and can’t do before you
+				drop off your clothing to be altered.
 			</h4>
 			<section className='service'>
 				<h2>Alterations</h2>
 				<div className='container'>
 					<div className='serviceContent'>
 						<p>
-							Although tailoring isn’t a great option if you’ve
-							dramatically changed clothing sizes, it is helpful if
-							you’ve changed by just one size. If you’ve recently gone up
-							or down a size, you don’t necessarily need to buy all-new
-							clothing. Keep your current wardrobe by getting your
-							favorite articles of clothing altered. I can let out or
-							take in the waist of your favorite garments. I can even
-							adjust the sizes of sleeves and dress straps! Maybe you
-							haven’t changed sizes, but you still need to adjust your
-							clothing to make it fit perfectly. An experienced tailor is
-							the only one that can help you! Whether you need help with
-							your wedding dress, formal gown, or any other article of
-							clothing, come to our tailor shop!
+							Although tailoring isn’t a great option if
+							you’ve dramatically changed clothing sizes,
+							it is helpful if you’ve changed by just one
+							size. If you’ve recently gone up or down a
+							size, you don’t necessarily need to buy
+							all-new clothing. Keep your current wardrobe
+							by getting your favorite articles of clothing
+							altered. I can let out or take in the waist
+							of your favorite garments. I can even adjust
+							the sizes of sleeves and dress straps! Maybe
+							you haven’t changed sizes, but you still need
+							to adjust your clothing to make it fit
+							perfectly. An experienced tailor is the only
+							one that can help you! Whether you need help
+							with your wedding dress, formal gown, or any
+							other article of clothing, come to our tailor
+							shop!
 						</p>
 					</div>
 					<span className='serviceImage'>
@@ -40,15 +45,18 @@ export default function Services() {
 				<div className='containerReverse'>
 					<div className='serviceContent'>
 						<p>
-							Have you ever torn a hole in your favorite pair of jeans or
-							blouse? This can be devastating, especially if it was an
-							expensive item or a garment that holds sentimental value.
-							You don’t have to throw them out. Simply take them to my
-							shop!
-							<br />I would be happy to patch up, sew, or mend your items
-							to restore their former glory. With my services, a small
-							tear doesn’t have to be the end of your favorite clothing
-							item. Just drop it off, and I’ll make it good as new!
+							Have you ever torn a hole in your favorite
+							pair of jeans or blouse? This can be
+							devastating, especially if it was an
+							expensive item or a garment that holds
+							sentimental value. You don’t have to throw
+							them out. Simply take them to my shop!
+							<br />I would be happy to patch up, sew, or
+							mend your items to restore their former
+							glory. With my services, a small tear doesn’t
+							have to be the end of your favorite clothing
+							item. Just drop it off, and I’ll make it good
+							as new!
 						</p>
 					</div>
 					<span className='serviceImage'>
@@ -61,18 +69,22 @@ export default function Services() {
 				<div className='container'>
 					<div className='serviceContent'>
 						<p>
-							Sometimes your clothing flatters your body shape perfectly,
-							but it’s a little long. Have no fear! Hemming is a cinch
-							for an experienced tailor! I can change the length of your
-							skirt, dress, jeans, or dress pants. When I’m done with
-							your hemming, you’ll be wearing clothing that’s the perfect
-							fit and length.
+							Sometimes your clothing flatters your body
+							shape perfectly, but it’s a little long. Have
+							no fear! Hemming is a cinch for an
+							experienced tailor! I can change the length
+							of your skirt, dress, jeans, or dress pants.
+							When I’m done with your hemming, you’ll be
+							wearing clothing that’s the perfect fit and
+							length.
 							<br />
-							Sometimes the exact opposite is true. Your clothing fits
-							nicely, but it just isn’t long enough. If you’re above
-							average height, this could be an ongoing life struggle.
-							Don’t settle for clothing that isn’t the ideal length. I
-							can make all your garments the perfect length!
+							Sometimes the exact opposite is true. Your
+							clothing fits nicely, but it just isn’t long
+							enough. If you’re above average height, this
+							could be an ongoing life struggle. Don’t
+							settle for clothing that isn’t the ideal
+							length. I can make all your garments the
+							perfect length!
 						</p>
 					</div>
 					<span className='serviceImage'>
@@ -85,13 +97,15 @@ export default function Services() {
 				<div className='containerReverse'>
 					<div className='serviceContent'>
 						<p>
-							At Atelier Prosto Mariya, I can fix your broken buttons and
-							zippers. I can also add them to your clothing, even if your
-							clothing never had them to begin with. Having buttons or
-							zippers on your clothing can help you adjust your dresses,
-							skirts, and more with ease. They make your attire look
-							extra stylish and help you slip into your clothing quickly
-							without the assistance of others.
+							At Atelier Prosto Mariya, I can fix your
+							broken buttons and zippers. I can also add
+							them to your clothing, even if your clothing
+							never had them to begin with. Having buttons
+							or zippers on your clothing can help you
+							adjust your dresses, skirts, and more with
+							ease. They make your attire look extra
+							stylish and help you slip into your clothing
+							quickly without the assistance of others.
 						</p>
 					</div>
 					<span className='serviceImage'>
@@ -104,18 +118,22 @@ export default function Services() {
 				<div className='container'>
 					<div className='serviceContent'>
 						<p>
-							Can’t find the exact clothing you’ve been looking for? I
-							can create custom clothing that will be the perfect
-							addition to your wardrobe. If you love wearing unique
-							clothing, you can’t get any more unique than a custom
-							dress, top, or other items from Atelier Prosto Mariya! My
-							custom clothing will help you feel confident and beautiful!
-							<br />I can also help you fall more in love with essential
-							pieces in your wardrobe. I can change the neckline on a
-							shirt or dress to ensure it’s as flattering as possible.
-							Just tell me your vision for your clothing, and I’ll try to
-							make it happen! I can remove the collars on collared
-							shirts, alter fur and leather items, and so much more.
+							Can’t find the exact clothing you’ve been
+							looking for? I can create custom clothing
+							that will be the perfect addition to your
+							wardrobe. If you love wearing unique
+							clothing, you can’t get any more unique than
+							a custom dress, top, or other items from
+							Atelier Prosto Mariya! My custom clothing
+							will help you feel confident and beautiful!
+							<br />I can also help you fall more in love
+							with essential pieces in your wardrobe. I can
+							change the neckline on a shirt or dress to
+							ensure it’s as flattering as possible. Just
+							tell me your vision for your clothing, and
+							I’ll try to make it happen! I can remove the
+							collars on collared shirts, alter fur and
+							leather items, and so much more.
 						</p>
 					</div>
 					<span className='serviceImage'>
@@ -123,14 +141,17 @@ export default function Services() {
 					</span>
 				</div>
 			</section>
-			<h4>Use Professional Tailoring Services from Atelier Prosto Mariya</h4>
+			<h4>
+				Use Professional Tailoring Services from Atelier Prosto
+				Mariya
+			</h4>
 			<p>
-				Don’t ever attempt to alter or repair a garment yourself. The
-				results could be irreversible. Leave all your garment repair needs
-				to the real experts Atelier Prosto Mariya! Call today to make an
-				appointment at the tailor shop.
+				Don’t ever attempt to alter or repair a garment yourself.
+				The results could be irreversible. Leave all your garment
+				repair needs to the real experts Atelier Prosto Mariya! Call
+				today to make an appointment at the tailor shop.
 			</p>
 			<hr className='major' />
-		</div>
+		</section>
 	);
 }
